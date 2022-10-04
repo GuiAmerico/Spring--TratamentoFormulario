c28b897c2c53b8f4912d5dadcf744c5764394f17
Squad 048 <109047518+Squad048@users.noreply.github.com>
2022-10-04 15:34:36 -0300

    adicionando classes para malipulação de formulario com Spring

---
+++ SpringTratamentoFormulario/src/test/java/com/spring/tratamentoformulario/SpringTratamentoFormularioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.tratamentoformulario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTratamentoFormularioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
